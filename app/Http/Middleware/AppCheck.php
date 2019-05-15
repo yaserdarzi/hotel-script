062cc8e698122d59d44ba1dc962197f68b7c931a
yaser <yaser.darzi@gmail.com>
2019-05-15 17:40:42 +0430

    add migrate and model

--- app/Http/Middleware/AppCheck.php
+++ app/Http/Middleware/AppCheck.php
@@ -32,56 +32,38 @@ class AppCheck
                 'Plz check your appToken header'
             );
         $appToken = JWT::decode($request->header('appToken'), config("jwt.secret"), array('HS256'));
-        if ($appToken->app != Constants::APP || $appToken->type_app != Constants::TYPE_APP)
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => env('CDN_AUTH_URL') . "/api/v1/apiChecker",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_POSTFIELDS => "",
+            CURLOPT_HTTPHEADER => array(
+                "Accept: application/json",
+                "Postman-Token: 014f0d51-e8a3-4346-babd-9e6ea9e7aef2",
+                "cache-control: no-cache",
+                "apiId: " . $appToken->api_id
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+        if ($err)
             throw new ApiException(
                 ApiException::EXCEPTION_BAD_REQUEST_400,
-                'Plz check your appToken header'
+                $err
             );
-        if ($appToken->type_app_child == "") {
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => env('CDN_AUTH_URL') . "/api/v1/app",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-                CURLOPT_POSTFIELDS => "",
-                CURLOPT_HTTPHEADER => array(
-                    "Accept: application/json",
-                    "Postman-Token: 014f0d51-e8a3-4346-babd-9e6ea9e7aef2",
-                    "app: " . $appToken->app,
-                    "cache-control: no-cache",
-                    "typeApp: " . $appToken->type_app
-                ),
-            ));
-            $response = curl_exec($curl);
-            $err = curl_error($curl);
-            $info = curl_getinfo($curl);
-            curl_close($curl);
-            if ($err)
-                throw new ApiException(
-                    ApiException::EXCEPTION_BAD_REQUEST_400,
-                    $err
-                );
-            if ($info['http_code'] != 200)
-                throw new ApiException(
-                    ApiException::EXCEPTION_BAD_REQUEST_400,
-                    json_decode($response)->error
-                );
-            $input['app_id'] = json_decode($response)->data->app_id;
-        } else {
-            if (!in_array($appToken->type_app_child, Constants::TYPE_APP_CHILD))
-                throw new ApiException(
-                    ApiException::EXCEPTION_BAD_REQUEST_400,
-                    'Plz check your appToken header'
-                );
-            $input['app_id'] = [$appToken->app_id];
-        }
-        $input['app'] = $appToken->app;
-        $input['type_app'] = $appToken->type_app;
-        $input['type_app_child'] = $appToken->type_app_child;
+        if ($info['http_code'] != 200)
+            throw new ApiException(
+                ApiException::EXCEPTION_BAD_REQUEST_400,
+                json_decode($response)->error
+            );
+        $input['app_id'] = json_decode($response)->data->app_id;
         $request->replace($input);
         return $next($request);
     }
