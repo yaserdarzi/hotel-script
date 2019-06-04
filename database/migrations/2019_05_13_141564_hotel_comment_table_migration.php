64ee41aade2c3d9ca3247ee528a63801ed780336
yaser <yaser.darzi@gmail.com>
2019-06-04 12:00:36 +0430

    fix hotel comment

--- database/migrations/2019_05_13_141564_hotel_comment_table_migration.php
+++ database/migrations/2019_05_13_141564_hotel_comment_table_migration.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SupplierCommentTableMigration extends Migration
+class HotelCommentTableMigration extends Migration
 {
     /**
      * Run the migrations.
